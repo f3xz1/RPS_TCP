09adbe299ff035822c5118a7be9c0d278494e1c3
f3xz1 <dgagnidzeh@gmail.com>
2023-04-06 22:18:20 +0400

    ez

---
+++ RPS_Listener/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+TcpListener tcpListener = new(IPAddress.Any,1337);
+
+tcpListener.Start();
+Random random = new Random();
+var client = await tcpListener.AcceptTcpClientAsync();
+Console.WriteLine(client.Client.LocalEndPoint);
+var ClientStream = client.GetStream();
+
+while (true)
+{
+    var buffer = new byte[4];
+
+    var result = await ClientStream.ReadAsync(buffer,0, buffer.Length);
+
+    Console.WriteLine();
+
+    buffer = BitConverter.GetBytes(random.Next(0,3));
+
+    await ClientStream.WriteAsync(buffer,0,result);
+}
---
+++ RPS_TCP/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.ComponentModel;
+using System.Net.Sockets;
+using System.Text;
+using System.Xml.Serialization;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+    string[] options = { "Rock", "Paper", "Scissors" };
+        TcpClient tcpClient = new TcpClient("localhost",1337);
+
+        var stream = tcpClient.GetStream();
+
+        Console.WriteLine("Connected to : " + tcpClient.Connected);
+
+        int ClientChoice;
+        int ServerChoice;
+
+        int ClientScore = 0;
+        int ServerScore = 0;
+
+
+        while (true)
+        {
+
+            await Console.Out.WriteLineAsync($"\nТы: {ClientScore}\nСоперник: {ServerScore}");
+
+            Console.WriteLine("\n 1.Камень\n 2.Бумага\n 3.Ножницы");
+            try
+            {
+                ClientChoice = int.Parse(Console.ReadLine())-1;
+
+                var bufferClient = BitConverter.GetBytes(ClientChoice);
+
+                await stream.WriteAsync(bufferClient, 0, bufferClient.Length);
+
+                var bufferServer = new byte[4];
+                await stream.ReadAsync(bufferServer,0,bufferServer.Length);
+
+
+                ServerChoice = BitConverter.ToInt32(bufferServer);
+
+                Console.Clear();
+
+                await Console.Out.WriteLineAsync($"\nСоперник выбрал {options[ServerChoice]}");
+
+                await Console.Out.WriteLineAsync($"\nВы выбрали выбрал {options[ClientChoice]}");
+
+
+                Console.WriteLine();
+                if (options[ServerChoice] == options[ClientChoice])
+                {
+                    await Console.Out.WriteLineAsync("Ничья");
+                }
+                else if ((ClientChoice == 0 && ServerChoice == 2) ||
+                         (ClientChoice == 1 && ServerChoice == 0) ||
+                         (ClientChoice == 2 && ServerChoice == 1))
+                {
+                    await Console.Out.WriteLineAsync("Победа!");
+                    ClientScore++;
+                }
+                else
+                {
+                    await Console.Out.WriteLineAsync("Поражение");
+                    ServerScore++;
+                }
+                if(ServerScore == 3 || ClientScore == 3)
+                {
+                    await Console.Out.WriteLineAsync($"\nТы: {ClientScore}\n Соперник: {ServerScore}");
+                    await Console.Out.WriteLineAsync("Game Over");
+                    break;
+                }
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Введите номер!");
+                Console.WriteLine(e.Message);
+            }
+        }
+        stream.Close();
+    }
+}
